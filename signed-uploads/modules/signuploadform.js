6e6add0119271341790147655bfd24d244071ac3
tersickpan <sickfan21@gmail.com>
2025-08-29 06:57:41 +0800

    changes signed params

--- signed-uploads/modules/signuploadform.js
+++ signed-uploads/modules/signuploadform.js
@@ -2,13 +2,14 @@ const cloudinary = require("../config/cloudinary");
 
 // Server-side function used to sign an upload with a couple of
 // example eager transformations included in the request.
-const signuploadform = () => {
+const signuploadform = ({ folder, entryKey }) => {
   const timestamp = Math.round(new Date().getTime() / 1000);
 
   const signature = cloudinary.utils.api_sign_request(
     {
       timestamp: timestamp,
-      folder: "test_uploads",
+      asset_folder: folder,
+      public_id: entryKey,
     },
     process.env.CLOUDINARY_API_SECRET
   );
--- signed-uploads/routes/signuploadform.js
+++ signed-uploads/routes/signuploadform.js
@@ -4,7 +4,8 @@ const signature = require("../modules/signuploadform");
 
 // using this API should require authentication
 router.get("/", function (req, res, next) {
-  const sig = signature.signuploadform();
+  const params = req.query;
+  const sig = signature.signuploadform(params);
   res.json({
     signature: sig.signature,
     timestamp: sig.timestamp,
