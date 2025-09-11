3894cd893d86bf09f2bc80594161ef00c9c9f69f
tersickpan <sickfan21@gmail.com>
2025-09-12 05:50:07 +0800

    removing public_id from input

--- signed-uploads/modules/signuploadform.js
+++ signed-uploads/modules/signuploadform.js
@@ -2,14 +2,13 @@ const cloudinary = require("../config/cloudinary");
 
 // Server-side function used to sign an upload with a couple of
 // example eager transformations included in the request.
-const signuploadform = ({ folder, entryKey }) => {
+const signuploadform = ({ folder }) => {
   const timestamp = Math.round(new Date().getTime() / 1000);
 
   const signature = cloudinary.utils.api_sign_request(
     {
       timestamp: timestamp,
       asset_folder: folder,
-      public_id: entryKey,
     },
     process.env.CLOUDINARY_API_SECRET
   );
