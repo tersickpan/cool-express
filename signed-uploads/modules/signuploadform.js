d0be6e56aafb8e1ac158a2966ed2728d78c25d19
tersickpan <sickfan21@gmail.com>
2025-09-19 01:26:58 +0800

    added every param to sign

--- signed-uploads/modules/signuploadform.js
+++ signed-uploads/modules/signuploadform.js
@@ -2,13 +2,15 @@ const cloudinary = require("../config/cloudinary");
 
 // Server-side function used to sign an upload with a couple of
 // example eager transformations included in the request.
-const signuploadform = ({ folder }) => {
+const signuploadform = ({ folder, entryKey }) => {
   const timestamp = Math.round(new Date().getTime() / 1000);
 
   const signature = cloudinary.utils.api_sign_request(
     {
       timestamp: timestamp,
       asset_folder: folder,
+      public_id: entryKey,
+      display_name: entryKey,
     },
     process.env.CLOUDINARY_API_SECRET
   );
